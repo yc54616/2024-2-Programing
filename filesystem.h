7d71c18e2d7e1c35a360a5f7f49e282fe9001b0d
In-Sam <yanghongjang@naver.com>
2024-10-30 13:32:04 +0900

    filesystem

--- filesystem.h
+++ filesystem.h
@@ -1,25 +1,39 @@
-    typedef struct _SuperBlock
-    {
-        bool inode_list_bit[128];
-        bool data_block_bit[256];
-    } SuperBlock;
-    typedef struct _InodeList
-    {
-        bool type; 
-        Date date;
-        int size;   
-    } InodeList;
-    typedef struct _Date
-    {
-            int year;
-            int month;
-            int day;
-            int hour;
-            int minute;
-            int second;
-    } Date;
-    
-    typedef struct _DataBlock
-    {
-        
-    } DataBlock;
+typedef struct {
+	unsigned char b0 : 1, b1 : 1, b2 : 1, b3 : 1, b4 : 1, b5 : 1, b6 : 1, b7 : 1; 
+} Byte;
+
+typedef struct {
+	Byte inode_list_bit[16]; // 8 * 16 bit = 128 bit
+	Byte data_block_bit[32]; // 8 * 32 bit = 256 bit
+} SuperBlock;
+typedef struct {
+	int year;
+	int month;
+	int day;
+	int hour;
+	int minute;
+	int second;
+} Date;
+typedef struct {
+	Byte typeAndDorS; // Data type ( directory or general file ) and ( Direct or Single Indirect )
+	//                       0                                           0000000
+	Date date;
+	unsigned int size;
+	union {
+		unsigned char direct[8];
+		unsigned char single;
+	};
+} InodeList;
+
+typedef union {
+    unsigned char subfiles[32][8]; // Case it is directory
+    // 7 bytes : name 1 byte : inode
+    unsigned char contents[256];   // Case it is general file
+} DataBlock;
+
+typedef struct {
+	Byte boots[2];
+	SuperBlock superBlock;
+	InodeList Ilist[384];
+	DataBlock DBlock[256];
+} FileSystem;
