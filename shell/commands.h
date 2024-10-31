95f03b5cd2264b6d496b12ad2e9ba8aa5429b71a
red <jchoe1389@gmail.com>
2024-10-31 20:56:16 +0900

    shell디렉토리를 만들고 shell.c를 main, system, commands로 분할하여 저장하였다.

---
+++ shell/commands.h
@@ -0,0 +1,24 @@
+/*
+exit을 제외한 명령어들 각각을 함수로써 저장하였다.
+
+*/
+void print1(void) // 테스트용 함수
+{
+    printf("1 1 1 1\n");
+    return;
+}
+void print2(void)
+{
+    printf("2 2 2 2\n");
+    return;
+}
+void clear(void)
+{
+    /**
+ @brief clear. bash에 clear명령어 실행
+ @param void
+ @return void
+ */
+    system("clear");
+    return;
+}
