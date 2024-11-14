a3029843f6be03b9d386f60fc2a00412ad14c421
HooJooHoo <jh_050@naver.com>
2024-11-14 14:01:04 +0900

    중간 저장

--- hamjoohyuk_commands.c
+++ hamjoohyuk_commands.c
@@ -5,6 +5,122 @@
 #include <string.h>
 #include "file_system.h"
 #include "io_stream.h"
+#include "data_struct.h"
+
+extern chainedDirectory *working_directory;
+time_t current_time;
+
+int getNeededDirectAdressNumber(char* entire_contents) {
+    if (strlen(entire_contents) > 256 * 8) { 
+        return 9; //single indirect adress 필요
+    }
+    return strlen(entire_contents) / 256 + 1;
+}
+
+int allocateInodeForNewFiles(char* new_file_name, int direct_adress_number) {
+    SuperBlock super_block = getSuperBlock();
+	InodeList inode_list;
+    DataBlock data_block;
+    unsigned char new_inode_list_index = findEmptyInode();
+    unsigned char new_data_block_index[8] = {0};
+    setSuperBlock(new_inode_list_index, 1);
+
+    for(int i = 0; i < direct_adress_number; i++) {
+        new_data_block_index[i] = findEmptyDataBlock();
+        setSuperBlock(128 + new_data_block_index[i] + 1, 1);
+    }
+
+    setInodeList(new_inode_list_index, 0, 1, 1, 1, direct_adress_number, new_data_block_index, 0);
+    return new_inode_list_index;
+}
+
+void writeFileContents(char* entire_contents, int inode_list_adress, int direct_adress_number) { 
+    SuperBlock super_block = getSuperBlock();
+	InodeList inode_list = getInodeList(inode_list_adress);
+    DataBlock data_block;
+    char data_block_adress[8] = {0};
+    unsigned char divided_contents[8][256] = {0};  // 8개의 256바이트 배열 선언(Direct Block)
+    unsigned char new_contents[256] = {0};
+
+    for (int i = 0; i < direct_adress_number && i * 256 < strlen(entire_contents); i++) {  // 256바이트씩 나눠서 배열에 복사
+        strncpy(divided_contents[i], entire_contents + i * 256, 256);
+    }
+
+    for(int i = 0; i < direct_adress_number; i++) {
+        data_block_adress[i] = inode_list.direct_address[i];
+        if(divided_contents[i] != NULL && strlen(divided_contents[i]) != 0) {
+                for(int j = 0; j < 256; j++) new_contents[j] = divided_contents[i][j];
+                setDataBlock(data_block_adress[i], new_contents);
+                data_block = getDataBlock(data_block_adress[i]); 
+                continue;
+        }
+    }
+}
+
+char getAllDirectAdressWithSourceFileName(char* source_file, int result_index) {
+    SuperBlock super_block = getSuperBlock();
+	InodeList inode_list;
+    DataBlock data_block;
+    int inode_list_of_file = 0;
+    bool is_source_file_name_exist = false;
+    for(int i = 0; i < SIZE_DATABLOCK; i++) {
+        data_block = getDataBlock(i);
+        for(int j = 0; j < 32; j++) {
+            for(int k = 0; k < 7; k++) {
+                if(source_file[k] == data_block.subfiles[j][k]) {
+                    if(k == 7) {
+                        inode_list_of_file = data_block.subfiles[j][8];
+                        is_source_file_name_exist = true;
+                    }
+                }
+            }
+    }
+    }
+
+    if(is_source_file_name_exist == false) {
+        return 'e'; //임시
+    }
+
+    inode_list = getInodeList(inode_list_of_file);
+    return inode_list.direct_address[result_index];
+
+}
+
+char* getFileContentsWithSourceFileName(char* source_file) {
+    SuperBlock super_block = getSuperBlock();
+	InodeList inode_list;
+    DataBlock data_block;
+    int inode_list_of_file = 0;
+    char* file_content = malloc(sizeof(char) * (256 * 8));
+    bool is_source_file_name_exist = false;
+    for(int i = 0; i < SIZE_DATABLOCK; i++) {
+        data_block = getDataBlock(i);
+        for(int j = 0; j < 32; j++) {
+            for(int k = 0; k < 7; k++) {
+                if(source_file[k] == data_block.subfiles[j][k]) {
+                    if(k == 7) {
+                        inode_list_of_file = data_block.subfiles[j][8];
+                        is_source_file_name_exist = true;
+                    }
+                }
+            }
+    }
+    }
+
+    if(is_source_file_name_exist == false) {
+        return "error"; //임시
+    }
+
+    inode_list = getInodeList(inode_list_of_file);
+    for(int i = 0; i < 8; i++) {
+        data_block = getDataBlock(inode_list.direct_address[i]);
+        strcat(file_content, data_block.contents);
+    }
+    printf("%s", file_content); //test
+    return file_content;
+
+}
+
 
 void myshowfile(int num1, int num2, char* file_name) {
     char* entire_contents = getFileContentsWithSourceFileName(file_name); 
@@ -20,80 +136,93 @@ void myshowfile(int num1, int num2, char* file_name) {
     printf("\n");
 }
 
-char* getFileContentsWithSourceFileName(char* source_file) { //source_file이 DataBlock의 contents 멤버 안에 존재하면 해당 파일의 InodeList의 directs 멤버에 해당하는 모든 contents 내용 return, 예외 발생 시 error return 
+void myrm(char* file_name) {
     SuperBlock super_block = getSuperBlock();
 	InodeList inode_list;
     DataBlock data_block;
-    int inode_list_index = 0, index_of_data_block = 0, index_of_contents = 0;
-    char* file_content = malloc(sizeof(char) * (256 * 8));
-    bool is_source_file_name_exist = false;
-    for(int i = 0; i < SIZE_DATABLOCK; i++) {
-        data_block = getDataBlock(i);
-        for(int j = 0; j < strlen(source_file); j++) {
-            if(data_block.contents[j] == source_file[j]) {
-                if(j == strlen(source_file) - 1) {
-                    is_source_file_name_exist = true;
-                    index_of_data_block = i;
-                    index_of_contents = j;
-                }
-                continue;
-            }
-            else break;
-            }
+    char direct_adress[8] = {0};
+    
+    for(int i = 0; i < 8; i++) { 
+        direct_adress[i] = getAllDirectAdressWithSourceFileName(file_name, i);
     }
 
-    if(is_source_file_name_exist == false) {
-        return "error"; //임시
+    for(int i = 0; i < 8; i++) {
+        setDataBlock(direct_adress[i], NULL);
+        setSuperBlock(direct_adress[i] + 128, 0);
     }
+}   
 
-    for(int k = 0; k < SIZE_INODELIST; k++) {
-        inode_list = getInodeList(k + 1); //Inode는 1부터 시작
-        if(inode_list.file_mode == 0) { //0 == file, 1 == directory
-            for(int l = 0; l < 8; l++) {
-                data_block = getDataBlock(inode_list.direct_address[l]);
-                strcat(file_content, data_block.contents);
-            }
-            printf("%s", file_content); //test
-            return file_content;
-        }
-        else return "error"; //임시
-    }
-}
-
-void writeFileContents(char* entire_contents) { //긴 문자열을 나
+void mycpfrom(char* host_source_file, char* dest_file) {
     SuperBlock super_block = getSuperBlock();
 	InodeList inode_list;
     DataBlock data_block;
-    int len = strlen(entire_contents);
-    
-    if (len > 256 * 6) {
-        return; //임시 에러 처리 
-    }
-    
-    char devided_contents[8][256];  // 8개의 256바이트 배열 선언(Direct Block)
-    
-    for (int i = 0; i < 8 && i * 256 < len; i++) {  // 256바이트씩 나눠서 배열에 복사
-        strncpy(devided_contents[i], entire_contents + i * 256, 256);
+    FILE *host_txt;
+    char *host_content = {0};
+    int host_txt_size;
+    char *buffer;
+    int file_count;
+    int inode_list_address_of_dest_file = 0;
+
+    host_txt = fopen(host_source_file, "r");
+    if(host_txt == NULL) {
+        printf("그런 호스트 파일이 없습니다.");
+        return ;
     }
 
-    for (int i = 0; i < 8; i++) { 
-        printf("devided_contents[%d]: %s\n", i, devided_contents[i]); //test
-        for (int j = 0; j < 256; j++) {
-            data_block = getDataBlock(j);
-            if(data_block.contents == NULL && devided_contents[i] != NULL){
-                setDataBlock(j, devided_contents[i]);
-                //TODO::SuperBlock, InodeList값도 변경
-                break;
-            }
+    fseek(host_txt, 0, SEEK_END);    
+    host_txt_size = ftell(host_txt);   
+    buffer = malloc(host_txt_size + 1);
+    memset(buffer, 0, host_txt_size + 1); 
 
+    fseek(host_txt, 0, SEEK_SET);
+    file_count = fread(buffer, host_txt_size, 1, host_txt);    
+
+    getNeededDirectAdressNumber(buffer);
+    inode_list_address_of_dest_file = allocateInodeForNewFiles(dest_file, getNeededDirectAdressNumber(buffer));
+    writeFileContents(buffer, inode_list_address_of_dest_file, getNeededDirectAdressNumber(buffer));
+
+//test---------------
+    inode_list = getInodeList(inode_list_address_of_dest_file);
+    for(int i = 0; i < 8; i++) {
+        data_block = getDataBlock(inode_list.direct_address[i]);
+        for(int j = 0; j < 256; j++){
+            
+            printf("%c", data_block.contents[j]);
         }
-
+        printf("\n"); 
     }
+    //-------------------
+    fclose(host_txt);   
+    free(buffer);  
 
+    // inode_list = getInodeList(working_directory.my_inode_number);
+    // setInodeList(working_directory.my_inode_number, 1, inode_list.birth_date, 1, 1, inode_list.reference_count + 1,  inode_list.direct_address, inode_list.single_indirect_address);
     
 }
 
 int main() {
+    SuperBlock super_block = getSuperBlock();
+	InodeList inode_list;
+    DataBlock data_block;
+    working_directory -> 
+    printf("%s", working_directory -> my_name);
+    /*char* test_257_byte = "12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567\x00";
+    int inode_list_adress = allocateInodeForNewFiles("filename.txt", getNeededDirectAdressNumber(test_257_byte));
+    writeFileContents(test_257_byte, inode_list_adress, getNeededDirectAdressNumber(test_257_byte));
+    inode_list = getInodeList(inode_list_adress);
+    for(int i = 0; i < 8; i++) {
+        data_block = getDataBlock(inode_list.direct_address[i]);
+        for(int j = 0; j < 256; j++){
+            
+            printf("%c", data_block.contents[j]);
+        }
+        printf("\n");
+        
+    }*/
+   
+   //mycpfrom("test_host.txt", "test");
+
+
     //printf("%s", getFileContentsWithSourceFileName("")); //파일 이름 삽입
-    writeFileContents("");
+    //writeFileContents("");
 }
